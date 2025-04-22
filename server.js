3b25f3e3c5f7f4bd9cf461a28c608c51b68012aa
Tuhfe <158339151+Tuhfe@users.noreply.github.com>
2025-04-22 04:32:51 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = process.env.PORT;
+
+app.get('/api/news', async (req, res) => {
+  try {
+    const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=' + process.env.NEWS_API_KEY);
+    res.json(response.data.articles);
+  } catch (error) {
+    res.status(500).json({ error: 'API failed' });
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
