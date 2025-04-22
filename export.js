b87ffc138288ce6a1f33f3b951fdd9feaee0cc49
Tuhfe <158339151+Tuhfe@users.noreply.github.com>
2025-04-22 03:35:42 +0300

    Update export.js

--- export.js
+++ export.js
@@ -1,7 +1,7 @@
 function exportData(data) {
-    const blob = new Blob([JSON.stringify(data)], { type: 'application/json' });
-    const link = document.createElement('a');
+    const blob = new Blob([JSON.stringify(data)], { type: "application/json" });
+    const link = document.createElement("a");
     link.href = URL.createObjectURL(blob);
-    link.download = 'veri.json';
+    link.download = "exportedData.json";
     link.click();
 }
