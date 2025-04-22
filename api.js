0252ff017d13ee9cf68d6c6655cc61ec4138df25
Tuhfe <158339151+Tuhfe@users.noreply.github.com>
2025-04-22 03:16:25 +0300

    Update api.js

--- api.js
+++ api.js
@@ -1,8 +1,11 @@
 const apiKey = "1a6bfcf1ba4a4947b4736aabc0e5d42f"; // 
 const apiUrl = "https://newsapi.org/v2/top-headlines"; // 
 
-async function fetchData(region) {
-  const response = await fetch(`${apiUrl}?country=${region}&apiKey=${apiKey}`);
-  const data = await response.json();
-  return data;
+function fetchData(region, lang, startDate, endDate) {
+    const url = `${apiUrl}?region=${region}&lang=${lang}&start=${startDate}&end=${endDate}&apikey=${apiKey}`;
+    
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => data)
+        .catch(error => console.error("API verisi alınırken hata oluştu:", error));
 }
