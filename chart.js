3e864311b6c07493c441334883990072d2ba21d6
Tuhfe <158339151+Tuhfe@users.noreply.github.com>
2025-04-22 07:46:56 +0300

    Update chart.js

--- chart.js
+++ chart.js
@@ -71,12 +71,12 @@ export function renderTrendChart(data) {
 function processData(data) {
     const ctx = document.getElementById("myChart").getContext("2d");
     const chart = new Chart(ctx, {
-        type: 'bar',  // Grafiğin türü
+        type: 'bar',  
         data: {
-            labels: data.labels,  // API'den gelen etiketler
+            labels: data.labels,  
             datasets: [{
                 label: "Analiz Sonuçları",
-                data: data.values,  // API'den gelen sayısal veriler
+                data: data.values,
                 backgroundColor: 'rgba(255, 99, 132, 0.2)', 
                 borderColor: 'rgba(255, 99, 132, 1)',
                 borderWidth: 1
