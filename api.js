7dd64f97d4ed41f35ee3cd9c2cfdbc2947b38c58
Tuhfe <158339151+Tuhfe@users.noreply.github.com>
2025-04-22 03:34:10 +0300

    Update api.js

--- api.js
+++ api.js
@@ -1,11 +1,20 @@
-const apiKey = "1a6bfcf1ba4a4947b4736aabc0e5d42f"; // 
-const apiUrl = "https://newsapi.org/v2/top-headlines"; // 
+const apiKey = "1a6bfcf1ba4a4947b4736aabc0e5d42f";
+const apiUrl = "https://newsapi.org/v2/everything";
 
 function fetchData(region, lang, startDate, endDate) {
-    const url = `${apiUrl}?region=${region}&lang=${lang}&start=${startDate}&end=${endDate}&apikey=${apiKey}`;
-    
+    const url = `${apiUrl}?q=${region}&from=${startDate.toISOString()}&to=${endDate.toISOString()}&language=${lang}&apiKey=${apiKey}`;
+
     return fetch(url)
-        .then(response => response.json())
-        .then(data => data)
-        .catch(error => console.error("API verisi alınırken hata oluştu:", error));
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to fetch data!');
+            }
+            return response.json();
+        })
+        .then(data => {
+            return data.articles;
+        })
+        .catch(error => {
+            console.error("Error fetching data:", error);
+        });
 }
